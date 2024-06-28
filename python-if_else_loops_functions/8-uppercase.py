e927553db7d1afb5f5804ba1b6b2c2db98823944
Wakhile Dlamini <w.dlamini@aalustudent.com>
2024-06-28 09:01:28 -0700

    p8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -5,7 +5,7 @@ def uppercase(string):
     for char in range(big):
         ascii_v1 = ord(string[char])
         if 97 <= ascii_v1 <= 122:
-            ascii_new = ascii_v1 - 32
+            ascii_v2 = ascii_v1 - 32
             result += chr(ascii_v2)
         else:
             result += chr(ascii_v1)
