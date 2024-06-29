47f95ff464c795be6e691e4c0879d686808986fc
Wakhile Dlamini <w.dlamini@aalustudent.com>
2024-06-29 09:26:44 -0700

    more12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,8 +3,10 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or not roman_string:
         return 0
 
-    roman_characters = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                 'C': 100, 'D': 500, 'M': 1000}
+    roman_characters = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+                 }
 
     full_tal = 0
     prev_hashtag = 0
