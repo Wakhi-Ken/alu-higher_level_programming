d7b9a55a3edc8128057668d21ad6795b9c800308
Wakhile Dlamini <w.dlamini@aalustudent.com>
2024-06-29 05:44:17 -0700

    data3

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list)
-    return my_list[:]
-    copy_my_list = my_list[:]
-    copy_my_list[idx] = element
-    return copy_my_list
+     if 0 <= idx < len(my_list):
+         copy_list = list(my_list)
+         copy_list[idx] = element
+        return copy_list
+     else:
+        return copy_list
