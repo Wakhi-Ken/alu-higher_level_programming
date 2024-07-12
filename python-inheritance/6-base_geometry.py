65b51cb8cb1c295bd5c70d02695bb1cf2e4043ae
Wakhi-Ken <w.dlamini@alustudent.com>
2024-07-12 11:24:46 -0700

    ih6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""geometry"""
+
+class BaseGeometry:
+    """with an area method."""
+    def area(self):
+        """Unimplemented area method."""
+        raise Exception("area() is not implemented")
