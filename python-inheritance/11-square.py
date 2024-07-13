08c4814d60856c8af4b86ebc25588178d8ec0ea3
Wakhi-Ken <w.dlamini@alustudent.com>
2024-07-13 03:06:58 -0700

    ih11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -6,10 +6,11 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """A class that inherits from Rectangle."""
     def __init__(self, size):
-        """Initalizes width and height to size."""
+        """Initializes width and height to size."""
         self.integer_validator("size", size)
         super().__init__(size, size)
+        self.__size = size
 
     def __str__(self):
         """Returns string representation of the Square."""
-        return "[Square] {:d}/{:d}".format(self.__width, self.__height)
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
