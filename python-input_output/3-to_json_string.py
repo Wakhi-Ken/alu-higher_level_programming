5a909603cfa592f9731c3997255197b95fd6032b
Wakhi-Ken <w.dlamini@alustudent.com>
2024-07-19 06:22:48 -0700

    in3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""to_json_string() function."""
+import json
+
+
+def to_json_string(my_obj):
+    """Gets the JSON representation of an object
+    """
+    return json.dumps(my_obj)
