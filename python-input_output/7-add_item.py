61a415a6da0271a7875264e18e34981e98f65142
Wakhi-Ken <w.dlamini@alustudent.com>
2024-07-19 07:42:31 -0700

    in12

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -10,8 +10,13 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 """a script that adds all arguments to a Python list
 """
 try:
+    """Load existing arguments from the JSON file"""
     args = load_from_json_file("add_item.json")
 except FileNotFoundError:
+    """If the file doesn't exist, initialize with an empty list"""
     args = []
+
+"""Append the command line arguments to the list"""
 args.extend(sys.argv[1:])
+"""Save the updated list to the JSON file"""
 save_to_json_file(args, "add_item.json")
