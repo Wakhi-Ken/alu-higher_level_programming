283aa7c63c29cf62009448b543329f2f8ba156dd
Wakhi-Ken <w.dlamini@alustudent.com>
2024-07-19 07:12:17 -0700

    in9

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-"""Defines a class_to_json function."""
+"""class_to_json function."""
 
 
 def class_to_json(obj):
-    """Gets the dictionary description of an object.
-
-    Args:
-        obj: Object to retrieve the dictionary from
-    Returns:
-        A dictionary description
+    """Gets the dictionary description of an object
     """
     return obj.__dict__
