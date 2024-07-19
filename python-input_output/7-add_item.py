a81e9d3daa039f324c385a8a08a3fc5ab589e84d
Wakhi-Ken <w.dlamini@alustudent.com>
2024-07-19 08:23:06 -0700

    in12

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,7 +8,7 @@ import json
 
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file =__import__('6-load_from_json_file').load_from_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 if __name__ == "__main__":
@@ -18,5 +18,5 @@ if __name__ == "__main__":
         args = load_from_json_file("add_item.json")
     except FileNotFoundError:
         args = []
-        args.extend(sys.argv[1:])
-        save_to_json_file(args, "add_item.json")
+    args.extend(sys.argv[1:])
+    save_to_json_file(args, "add_item.json")
