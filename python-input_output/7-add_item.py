9a3eb9e76440685035f8c704722858b5791bdf2d
Wakhi-Ken <w.dlamini@alustudent.com>
2024-07-19 08:19:10 -0700

    in12

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-"""Saves command line arguments to a file."""
-
+"""
+all arguments to a python list and saves to a json file
+"""
 
 import sys
+import json
+
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+load_from_json_file =__import__('6-load_from_json_file').load_from_json_file
 
 
-"""a script that adds all arguments to a Python list
-"""
-try:
-    """Load existing arguments from the JSON file"""
-    args = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    """If the file doesn't exist, initialize with an empty list"""
-    args = []
+if __name__ == "__main__":
+    filename = "add_item.json"
 
-"""Append the command line arguments to the list"""
-args.extend(sys.argv[1:])
-"""Save the updated list to the JSON file"""
-save_to_json_file(args, "add_item.json")
+    try:
+        args = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        args = []
+        args.extend(sys.argv[1:])
+        save_to_json_file(args, "add_item.json")
