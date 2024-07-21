22ec3463fb426020c8b4539ce50a35eedfb49c6b
Wakhi-Ken <w.dlamini@alustudent.com>
2024-07-21 05:27:32 -0700

    more3

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,6 @@
+-- Creates a hbtn_0d_2 database.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
+-- Creates a user_0d_2 user.
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- Grants SELECT privileges to user_0d_2.
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
