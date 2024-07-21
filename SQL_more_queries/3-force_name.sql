ac9d45eaa1f3e2323335e2ba1cb920960b6ee106
Wakhi-Ken <w.dlamini@alustudent.com>
2024-07-21 05:29:40 -0700

    more1

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,6 +1,7 @@
--- Creates a hbtn_0d_2 database.
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
--- Creates a user_0d_2 user.
+-- Creates a hbtn_0d_2 database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Creating a user with specific privileges
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
--- Grants SELECT privileges to user_0d_2.
-GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
+
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
