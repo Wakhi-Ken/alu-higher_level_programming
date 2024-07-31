261da716703d7c25830160cc16fd22b3eea19b87
Wakhi-Ken <w.dlamini@alustudent.com>
2024-07-31 10:38:21 +0200

    net0

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # shows size of the body of the response
-response=$(curl -sI "$1")
-content_length=$(echo "$response" | sed -ne 's/Content-Length: \([0-9]*\).*/\1/p')
-echo "$content_length"
+curl -sI "$1" | sed -ne 's/Content-Length: //p'
