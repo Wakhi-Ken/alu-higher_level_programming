08e5510b00d985b8502135eb27fa5fa36d6075a1
Wakhi-Ken <w.dlamini@alustudent.com>
2024-08-06 16:47:43 +0000

    states

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Lists State objects by name """
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+"""prints the State object with the name passed as argument from the database hbtn_0e_6_usa """
+if __name__ == '__main__':
+    url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]
+    )
+    engine = create_engine(url, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(State).filter_by(name=sys.argv[4]).first()
+    print(result.id if result else "Not found")
+    session.close()
