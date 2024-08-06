a69e6b20f003479e58c3b2d43c0a0a7cf9ca1b6d
Wakhi-Ken <w.dlamini@alustudent.com>
2024-08-06 16:38:24 +0000

    model

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-# Lists all State objects.
+"""  Lists all State objects"""
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+
+""""  a script that lists all State objects from the database hbtn_0e_6_usa """
 if __name__ == '__main__':
     url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]
