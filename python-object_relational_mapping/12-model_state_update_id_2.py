f009bdef171ed88daf50976ba091e3c811b8ad54
Wakhi-Ken <w.dlamini@alustudent.com>
2024-08-06 16:56:14 +0000

    states

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Updates a State objects """
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+"""script that changes the name of a State object from the database hbtn_0e_6_usa"""
+if __name__ == '__main__':
+    url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]
+    )
+    engine = create_engine(url, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(State).filter_by(id=2).first()
+    if result:
+        result.name = "New Mexico"
+    session.commit()
+    session.close()
